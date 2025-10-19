e49cb3865696c94b86a277511d3c0153b5516901
Pathan-Saif <itlt2307@glbitm.ac.in>
2025-10-20 02:38:41 +0530

    remove MailConfig

--- src/main/java/net/engineer/moodPlanner/config/MailConfig.java
+++ src/main/java/net/engineer/moodPlanner/config/MailConfig.java
@@ -31,33 +31,33 @@
 
 
 
-package net.engineer.moodPlanner.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
-
-@Configuration
-public class MailConfig {
-
-    @Bean
-    public JavaMailSender javaMailSender(@Value("${SPRING_MAIL_USERNAME}") String username,
-                                         @Value("${SPRING_MAIL_PASSWORD}") String password) {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.sendgrid.net");
-        mailSender.setPort(587);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        return mailSender;
-    }
-
-}
+//package net.engineer.moodPlanner.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//
+//import java.util.Properties;
+//
+//@Configuration
+//public class MailConfig {
+//
+//    @Bean
+//    public JavaMailSender javaMailSender(@Value("${SPRING_MAIL_USERNAME}") String username,
+//                                         @Value("${SPRING_MAIL_PASSWORD}") String password) {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.sendgrid.net");
+//        mailSender.setPort(587);
+//        mailSender.setUsername(username);
+//        mailSender.setPassword(password);
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        return mailSender;
+//    }
+//
+//}
